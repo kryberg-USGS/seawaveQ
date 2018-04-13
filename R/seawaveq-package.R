76a53fdd43979fc16f62cc9c08a528f953939edd
Ryberg <kryberg@igsklbcwlt1002.gs.doi.net>
2018-04-13 14:45:22 -0500

    Test

--- R/seawaveq-package.R
+++ R/seawaveq-package.R
@@ -10,7 +10,7 @@
 #' Package: \tab seawaveQ\cr
 #' Type: \tab Package\cr
 #' Version: \tab 1.0.0\cr
-#' Date: \tab 2013--12--15\cr
+#' Date: \tab 2013--12--13\cr
 #' License: \tab Unlimited | file LICENSE \cr
 #' LazyLoad: \tab yes\cr
 #' }
