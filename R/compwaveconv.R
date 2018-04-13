0e1f5a6c27ed10ea9d23179c9a9d84466fcba2d5
byork-USGS <byork@usgs.gov>
2018-04-13 16:38:04 -0400

    Update compwaveconv.R

--- R/compwaveconv.R
+++ R/compwaveconv.R
@@ -35,22 +35,22 @@
 #' # these example decimal dates represent days at points 0.25, 0.5, and 
 #' # 0.75 percent of the way through the year and the end of the year
 #' dseas <- c(0.25, 0.5, 0.75, 1)
-#' swave <- compwaveconv(cmaxt=0.483, jmod=2, hlife=4, mclass=1)
+#' swave <- compwaveconv(cmaxt = 0.483, jmod = 2, hlife = 4, mclass = 1)
 #' swave[floor(360 * dseas)]
-#' plot(seq(0,1,1/360),swave, typ="l")
-compwaveconv <- function(cmaxt, jmod, hlife, mclass=1) {
+#' plot(seq(0, 1, 1/360), swave, typ = "l")
+compwaveconv <- function(cmaxt, jmod, hlife, mclass = 1) {
   del <- 1 / 12
   txx <- seq(0, 1, 1 / 360)
   
   # phi is the decay rate corresponding with an approximate model 
   # half-life of 12 divided by phi months.
-  if(hlife==1)  {
+  if (hlife == 1)  {
     phi <- 12
-  } else if(hlife==2) { 
+  } else if (hlife == 2) { 
     phi <- 6
-  } else if(hlife==3) { 
+  } else if (hlife == 3) { 
     phi <- 4
-  } else if(hlife==4) {
+  } else if (hlife == 4) {
     phi <- 3 
   } else { 
       stop("Half life value of ", hlife, 
@@ -59,46 +59,46 @@ compwaveconv <- function(cmaxt, jmod, hlife, mclass=1) {
   
   # wtx is the pulse input function and is greater than 0 during 
   # specified application sesons(s) and 0 otherwise.
-  if(jmod==1) {
+  if (jmod == 1) {
     wtx <- c(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0)
     pkt <- 6 / 12
-  } else if(jmod==2) {
+  } else if (jmod == 2) {
     wtx <- c(0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0)
     pkt <- 7 / 12
-  } else if(jmod==3) {
+  } else if (jmod == 3) {
     wtx <- c(0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0)
     pkt <- 8 / 12
-  } else if(jmod==4) {
+  } else if (jmod == 4) {
     wtx <- c(0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0)
     pkt <- 9 / 12
-  } else if(jmod==5) { 
+  } else if (jmod == 5) { 
     wtx <- c(0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0)
     pkt <- 9 / 12
-  } else if(jmod==6) {
+  } else if (jmod == 6) {
     wtx <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0)
     pkt <- 9 / 12
-  } else if(jmod==7) {
+  } else if (jmod == 7) {
     wtx <- c(0.5, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0.5)
     pkt <- 5 / 12
-  } else if(jmod==8) {
+  } else if (jmod == 8) {
     wtx <- c(0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0.5, 0.5)
     pkt <- 5 / 12
-  } else if(jmod==9) {
+  } else if (jmod == 9) {
     wtx <- c(0, 0, 0, 1, 1, 0, 0, 0, 0, 0.5, 0.5, 0)
     pkt <- 5 / 12
-  } else if(jmod==10) {
+  } else if (jmod == 10) {
     wtx <- c(0, 0, 0, 1, 1, 0, 0, 0, 0.5, 0.5, 0, 0)
     pkt <- 5 / 12
-  } else if(jmod==11) {
+  } else if (jmod == 11) {
     wtx <- c(0.5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0.5)
     pkt <- 5 / 12
-  } else if(jmod==12) {
+  } else if (jmod == 12) {
     wtx <- c(0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0.5, 0.5)
     pkt <- 5 / 12
-  } else if(jmod==13) {
+  } else if (jmod == 13) {
     wtx <- c(0, 0, 0, 0, 1, 0, 0, 0, 0, 0.5, 0.5, 0)
     pkt <- 5 / 12
-  } else if(jmod==14) { 
+  } else if (jmod == 14) { 
     wtx <- c(0, 0, 0, 0, 1, 0, 0, 0, 0.5, 0.5, 0, 0)
     pkt <- 5 / 12
   } else { 
@@ -114,31 +114,31 @@ compwaveconv <- function(cmaxt, jmod, hlife, mclass=1) {
   }
   con <- rho / (1 - rho) * con
   z0xx <- z0xx * con
-  zmat <- matrix(nrow=length(txx), ncol=12)
-  pckm <- matrix(nrow=length(txx), ncol=12)
+  zmat <- matrix(nrow = length(txx), ncol = 12)
+  pckm <- matrix(nrow = length(txx), ncol = 12)
   ntot <- length(txx)
-  pckm[,1] <- c(txx <= del)
+  pckm[, 1] <- c(txx <= del)
   for (k in 2:12) {
-    pckm[,k] <- c(txx > (k - 1) * del & txx <= k * del)
+    pckm[, k] <- c(txx > (k - 1) * del & txx <= k * del)
   }
-  zmat[,1] <- rho^txx * (rho^(-replace(txx, txx > 1/12, 1/12)) - 1)
+  zmat[, 1] <- rho^txx * (rho^(-replace(txx, txx > 1/12, 1/12)) - 1)
   for (k in 2:12) {
     ztmp <- rep(0, ntot)
     for (j in 1:(k - 1)) {
-      ztmp[pckm[,j]] <- 0
+      ztmp[pckm[, j]] <- 0
     }
-    ztmp[pckm[,k]] <- 1 - rho^(txx[pckm[,k]] - (k - 1) * del)
+    ztmp[pckm[, k]] <- 1 - rho^(txx[pckm[, k]] - (k - 1) * del)
     if(k < 12) {
       for (j in (k + 1):12) {
-        ztmp[pckm[,j]] <- rho^(txx[pckm[,j]] - del * k) - 
-          rho^(txx[pckm[,j]] - (k - 1) * del)
+        ztmp[pckm[, j]] <- rho^(txx[pckm[, j]] - del * k) - 
+          rho ^ (txx[pckm[, j]] - (k - 1) * del)
       }
     }
-    zmat[,k] <- ztmp
+    zmat[, k] <- ztmp
   }
   sst <- z0xx
   for (k in 1:12) { 
-    sst <- sst + wtx[k] * zmat[,k]
+    sst <- sst + wtx[k] * zmat[, k]
   }
   sst <- sst/phi
   medxx <- (max(sst) + min(sst)) / 2
