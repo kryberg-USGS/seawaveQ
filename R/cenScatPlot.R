f2542cecad43a7637848b533ef879175341e22d1
byork-USGS <byork@usgs.gov>
2018-04-13 15:57:39 -0400

    Update cenScatPlot.R

--- R/cenScatPlot.R
+++ R/cenScatPlot.R
@@ -59,7 +59,7 @@
 #' cenScatPlot(IllRivValleyCty, pname = "04035", 
 #'             site = "05586100 Illinois River at Valley City, IL",
 #'             ylabel = "Simazine concentration, in micrograms per liter", 
-#'             legcex = 0.7, ylim = c(0,0.4), yaxs = "i", cex.lab = 0.9, 
+#'             legcex = 0.7, ylim = c(0, 0.4), yaxs = "i", cex.lab = 0.9, 
 #'             cex.axis = 0.9, xlim = c(as.Date("1996-01-01", "%Y-%m-%d"), 
 #'             as.Date("2012-01-01", "%Y-%m-%d")), xaxs = "i", xaxt = "n")
 #' axdates <- c("1996-01-01", "2000-01-01", "2004-01-01", "2008-01-01",
