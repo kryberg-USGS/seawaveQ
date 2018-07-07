011be7df9722becf6e12c9b7667dae3e6cc4686e
Karen Ryberg <kryberg@usgs.gov>
2018-07-07 16:50:35 -0500

    version 2.0.0
    New in version 2.0.0 for plotting models that use restricted cubic splines.

--- R/seawaveQPlots2.R
+++ R/seawaveQPlots2.R
@@ -1,8 +1,100 @@
+#' Internal function that generates plots of data and model results.
+#' 
+#' seawaveQPlots2 is usually called from within \code{\link{fitMod2}} but
+#' can be invoked directly.  It generates plots of data and model results, 
+#' as well as diagnostic plots, and returns the observed and predicted 
+#' concentrations so that users may plot the concentrations using 
+#' their own functions. This is the version for models that use
+#' restricted cubic splines
+#' @note The plotting position used for representing censored values in 
+#' the plots produced by \code{\link{seawaveQPlots2}} is an important 
+#' consideration for interpreting model fit.  Plotting values obtained by 
+#' using the censoring limit, or something smaller such as one-half of the 
+#' censoring limit, produce plots that are difficult to interpret if there 
+#' are a large number of censored values.  Therefore, to make the plots 
+#' more representative of diagnostic plots used for standard 
+#' (non-censored) regression, a method for substituting randomized 
+#' residuals in place of censored residuals was used.   If a 
+#' log-transformed concentration is censored at a particular limit, 
+#' \code{logC < L}, then the residual for that concentration is censored 
+#' as well, \code{logC - fitted(logC) < L - fitted(logC) = rescen}.  In 
+#' that case, a randomized residual was generated from a conditional 
+#' normal distribution \cr \cr
+#' \code{resran  <-  scl * qnorm(runif(1) * pnorm(rescen / scl))}, \cr \cr
+#' where scl is the scale parameter from the survival regression model, 
+#' \code{pnorm} is the R function for computing cumulative normal 
+#' probabilities, \code{runif} is the R function for generating a 
+#' random variable from the uniform distribution, and \code{qnorm} 
+#' is the R function for computing quantiles of the normal distribution.  
+#' Under the assumption that the model residuals are uncorrelated, 
+#' normally distributed random variables with mean zero and standard 
+#' deviation scl, the randomized residuals generated in this manner are an 
+#' unbiased sample of the true (but unknown) residuals for the censored 
+#' data.  This is an application of the probability integral transform 
+#' (Mood and others, 1974) to generate random variables from continuous 
+#' distributions. The plotting position used a censored concentration is 
+#' \code{fitted(logC) + resran}.  Note that each time a new model fit is 
+#' performed, a new set of randomized residuals is generated and thus the 
+#' plotting positions for censored values can change.
+
+#' @param stpars is a matrix of information about the best seawaveQ model
+#' for the concentration data, see \code{\link{examplestpars}}.
+#' @param cmaxt is the decimal season of maximum chemical concentration.
+#' @param tseas is the decimal season of each concentration value in 
+#' cdatsub.
+#' @param tseaspr is the decimal season date used to model concentration 
+#' using the continuous data set cavdat.
+#' @param tndlin is the decimal time centered on the midpoint of the trend
+#' for the sample data, cdatasub.
+#' @param tndlinpr is is the decimal time centered on the midpoint of the 
+#' trend for the continuous data, cavdat.
+#' @param cdatsub is the concentration data
+#' @param cavdat is the continuous (daily) ancillary data
+#' @param cavmat is a matrix containing the continuous ancillary 
+#' variables.
+#' @param clog is a vector of the base-10 logarithms of the concentration 
+#' data.
+#' @param centmp is a logical vector indicating which concentration values
+#' are censored.
+#' @param yrstart is the starting year of the analysis (treated as January
+#' 1 of that year).  
+#' @param yrend is the ending year of the analysis (treated as December 31
+#' of that year).  
+#' @param tyr is a vector of decimal dates for the concentration data
+#' @param tyrpr is a vector of decimal dates for the continuous ancillary
+#' varaibles.
+#' @param pnames is the parameter (water-quality constituents) to 
+#' analyze (if using USGS parameters, omit the the starting 'P', such as 
+#' "00945" for sulfate).  
+#' @param tanm is an a character identifier that names the trend 
+#' analysis run.  It is used to label output files.
+#' @param mclass has not been implemented yet, but will provide
+#' additional model options.
+#' @keywords dplot hplot
+#' @author Aldo V. Vecchia and Karen R. Ryberg
+#' @return a pdf file containing plots of the data and modeled 
+#' concentrations and regression diagnostic plots and a list containing
+#' the observed concentrations (censored and uncensored) and the predicted
+#' concentrations used for the plot.
+#' @export
+#' @references
+#' Mood, A.M., Graybill, F.A., and Boes, D.C., 1974, Introduction to the 
+#' theory of statistics (3d ed.): New York, McGraw-Hill, Inc., 564 p.
+#' @examples
+#' data(swData)
+#' myPlots <- seawaveQPlots(stpars=examplestpars, cmaxt=0.4808743, 
+#' tseas=exampletseas, tseaspr=exampletseaspr, tndlin=exampletndlin,
+#' tndlinpr=exampletndlinpr, cdatsub=examplecdatsub, cavdat=examplecavdat, 
+#' cavmat=examplecavmat, clog=exampleclog, centmp=examplecentmp, 
+#' yrstart=1995, yrend=2003, tyr=exampletyr, tyrpr=exampletyrpr, 
+#' pnames=c("04041"), tanm="examplePlots04041", mclass = 2, numknots = 4)
 seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr, 
                             tndrcs, tndrcspr, cdatsub, 
                             cavdat, cavmat, clog, centmp, yrstart, yrend, tyr, 
                             tyrpr, pnames, tanm, mclass = 2, numknots) {
-  graphfile <- paste(tanm, pnames, ".pdf", sep = "")
+  # produce plots for selected model
+  # set up output file for graphs 
+  # output graphs to a pdfgraphfile <- paste(tanm, pnames, ".pdf", sep = "")
   gmes <- paste("Plots saved to ", graphfile, ".", sep = "")
   message(gmes)
   pdf(graphfile, height = 11, width = 8.5)
@@ -13,6 +105,8 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
   hlife1 <- pckone - (mod1 - 1) * 4
   ipkt <- floor(360 * tseas)
   ipkt[ipkt == 0] <- 1
+  
+  # call function to compute seasonal wave
   wavexx <- compwaveconv(cmaxt, mod1, hlife1, mclass = 2)
   wavest <- wavexx[ipkt]
   intcpt <- rep(1, length(wavest))
@@ -20,7 +114,6 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
   ipktpr[ipktpr == 0] <- 1
   wavestpr <- wavexx[ipktpr]
   intcptpr <- rep(1, length(wavestpr))
-  # tndrcsprAll <- matrix(data = tndlinpr, nrow = length(tndlinpr), ncol = (numknots - 1))
  
   xmat <- cbind(intcpt, wavest, tndrcs)
   if (length(cdatsub[1, ]) > 6) {
@@ -45,15 +138,16 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
   cresx02 <- clog - xmat %*% partmp
   # discrete fitted data
   fitadjxdat <- xmat %*% partmp
-  scltmp2 <- stpars[1, 3]
   # standardized residuals
+  scltmp2 <- stpars[1, 3]
   cresx02std <- (cresx02)/scltmp2
   ncenx <- sum(centmp)
+  # replace censored residuals with 
+  # conditional normal random variables
   cresx02std[centmp] <- qnorm(runif(ncenx) * pnorm(cresx02std[centmp]))
   cadjx0 <- clog
   
   # First plot, observed and predicted concentrations and trend
-  
   ylow <- floor(min(c(cadjx0, fitadjx02, fitadjx12)) - 0.25)
   yup <- ceiling(max(c(cadjx0, fitadjx02, fitadjx12)) + 1)
   ytck <- 0.02 * (yup - ylow)
@@ -62,6 +156,7 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
   xtck <- 0.012 * (xup - xlow)
   ytmp <- cadjx0
  
+  # ts plot of data and model 
   sp95 <- 1.645 * scltmp2
   plot(c(tyr, tyrpr), c(ytmp, fitadjx02), type = "p", pch = "", 
        xaxs = "i", yaxs = "i", xaxt = "n", yaxt = "n", xlab = "", 
@@ -97,8 +192,8 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
          cex = 0.8, col = 2)
   text(xlow + 0.5 * (xup - xlow), yup - 7.5 * ytck, adj = 0, 
        cex = 0.7, "Fitted concentrations (SWAVE-CAV)")
-  lines(c(xlow + 0.45 * (xup - xlow), xlow + 0.49 * (xup - 
-                                                       xlow)), c(yup - 7.5 * ytck, yup - 7.5 * ytck), lwd = 1.5, 
+  lines(c(xlow + 0.45 * (xup - xlow), xlow + 0.49 * (xup - xlow)), 
+        c(yup - 7.5 * ytck, yup - 7.5 * ytck), lwd = 1.5, 
         col = 1)
   text(xlow + 0.5 * (xup - xlow), yup - 10 * ytck, adj = 0, 
        cex = 0.65, "Trend")
@@ -111,7 +206,6 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
         outer = FALSE, line = 3, cex = 0.8)
   
   # Second plot, fitted 95th percentile and median concentrations
-  
   ylow <- 0
   tmpmax <- ceiling(quantile(10^c(fitadjx02 + 1.96 * scltmp2), 
                              prob = 0.999) * 200)/200
@@ -152,9 +246,9 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
     yup <- 5
   }
   if (tmpmax > 5) {
-    yup <- ceiling(tmpmax/5) * 5
+    yup <- ceiling(tmpmax / 5) * 5
   }
-  ystp <- yup/5
+  ystp <- yup / 5
   ytck <- 0.02 * (yup - ylow)
   xlow <- yrstart
   xup <- yrend
@@ -192,7 +286,8 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
         outer = FALSE, line = 3, cex = 0.8)
   
   # Third plot, fitted concentrations vs measured concentrations
-  
+  # replace censored values with 
+  # conditional normal random variables
   cadjx0 <- clog
   cadjx0[centmp] <- fitadjxdat[centmp] + cresx02std[centmp] * 
     scltmp2
@@ -202,8 +297,8 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
   xlow <- ylow
   xup <- yup
   xtck <- 0.5 * ytck
-  plot(fitadjxdat, cadjx0, type = "p", pch = "", xlim = c(xlow, 
-                                                          xup), ylim = c(ylow, yup), xaxs = "i", yaxs = "i", xaxt = "n", 
+  plot(fitadjxdat, cadjx0, type = "p", pch = "", xlim = c(xlow, xup), 
+       ylim = c(ylow, yup), xaxs = "i", yaxs = "i", xaxt = "n", 
        yaxt = "n", xlab = "", ylab = "")
   points(fitadjxdat[centmp], cadjx0[centmp], pch = 1, cex = 1, 
          col = 2)
@@ -226,17 +321,17 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
   mtext(side = 3, line = 0.5, adj = 0.5, cex = 1, tanm)
   mtext("Fitted Concentration, in micrograms per liter", side = 1, 
         outer = FALSE, line = 1.5, cex = 0.8)
-  mtext("Measured Concentration, in micrograms per liter\n(censored \n data randomized)", 
+  mtext("Measured Concentration, in micrograms per liter\n(censored data randomized)", 
         side = 2, outer = FALSE, line = 3.5, cex = 0.8)
   
   # Fourth plot
-  
   xlow0 <- xlow
   xup0 <- xup
   ylow <- floor(min(c(cresx02std)) - 0.25)
   yup <- ceiling(max(c(cresx02std)) + 0.25)
   ytck <- 0.02 * (yup - ylow)
   for (rplt in 1:3) {
+    #  residuals versus fitted
     ytmpsr <- cresx02std
     if (rplt == 1) {
       xlow <- xlow0
@@ -244,6 +339,7 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
       xstp <- 1
       xtck <- 0.012 * (xup - xlow)
     }
+    #  residuals versus time
     if (rplt == 2) {
       xlow <- yrstart
       xup <- yrend
@@ -315,14 +411,16 @@ seawaveQPlots2 <- function(stpars, cmaxt, tseas, tseaspr, tndlin, tndlinpr,
   dectime = NULL
   if (sum(centmp) > 0) {
     censDat <- data.frame(cbind(dectime = tyr[centmp], rmk = "<", 
-                                val = 10^ytmp[centmp]), stringsAsFactors = FALSE)
+                                val = 10^ytmp[centmp]), 
+                          stringsAsFactors = FALSE)
     censDat$dectime <- as.numeric(censDat$dectime)
     censDat$val <- as.numeric(censDat$val)
     dimnames(censDat)[[2]][2] <- paste("R", pnames, sep = "")
     dimnames(censDat)[[2]][3] <- paste("P", pnames, sep = "")
   }
   uncensDat <- data.frame(cbind(dectime = tyr[!centmp], rmk = "", 
-                                val = 10^ytmp[!centmp]), stringsAsFactors = FALSE)
+                                val = 10^ytmp[!centmp]), 
+                          stringsAsFactors = FALSE)
   uncensDat$dectime <- as.numeric(uncensDat$dectime)
   uncensDat$val <- as.numeric(uncensDat$val)
   dimnames(uncensDat)[[2]][2] <- paste("R", pnames, sep = "")
