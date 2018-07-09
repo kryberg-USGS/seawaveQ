bfcea413e241ff0c5ee7aedcb686ccd4a7bff0ae
Ryberg <kryberg@usgs.gov>
2018-07-09 13:56:32 -0500

    r cmd check fixes

--- seawaveQ/R/seawaveq-package.R
+++ seawaveQ/R/seawaveq-package.R
@@ -34,11 +34,11 @@
 #' De Cicco, L.A., Mills, T.J., and Farmer, W.H., 2017, Water-quality trends 
 #' in the nation's rivers and streams, 1972--2012---Data preparation, statistical 
 #' methods, and trend results: U. S. Geological Survey Scientific Investigations 
-#' Report 2017--5006, 136 p., http://pubs.er.usgs.gov/publication/sir20175006.
+#' Report 2017--5006, 136 p.,  \url{https://doi.org/10.3133/sir20175006}.
 #'
 #' Ryberg, K.R. and Gilliom, R.J., 2015, Trends in pesticide concentrations 
 #' and use for major rivers of the United States: Science of The Total Environment, 
-#' v. 538, p. 431--444, https://doi.org/10.1016/j.scitotenv.2015.06.095.
+#' v. 538, p. 431--444,  \url{https://doi.org/10.1016/j.scitotenv.2015.06.095}.
 #'
 #' Ryberg, K.R. and Vecchia, A.V., 2013, seawaveQ---An R package providing 
 #' a model and utilities for analyzing trends in chemical concentrations 
@@ -65,10 +65,10 @@
 #' Vecchia, A.V., Gilliom, R.J., Sullivan, D.J., Lorenz, D.L., and 
 #' Martin, J.D., 2009, Trends in concentrations and use of agricultural 
 #' herbicides for Corn Belt rivers, 1996--2006:  Environmental Science 
-#' and Technology, v. 43, p. 9,096-9,102.
+#' and Technology, v. 43, p. 9,096--9,102.
 #'
 #' Vecchia, A.V., Martin, J.D., and Gilliiom, R.J., 2008, Modeling 
 #' variability and  trends in pesticide concentrations in streams: 
 #' Journal of the American Water Resources Association, v. 44, no. 5, p. 
-#' 1308-1324, \url{http://dx.doi.org/10.1111/j.1752-1688.2008.00225.x}.
+#' 1308--1324, \url{http://dx.doi.org/10.1111/j.1752-1688.2008.00225.x}.
 NULL
